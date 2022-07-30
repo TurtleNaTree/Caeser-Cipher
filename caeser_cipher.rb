27f00f5199474d71fcaa178c6739027d1cded8ef
TurtleNaTree <kmcqueen108121@hotmail.com>
2022-07-30 09:24:57 +0000

    added code for user input and validation

--- caeser_cipher.rb
+++ caeser_cipher.rb
@@ -12,5 +12,23 @@ def caeser_cipher(text, shift)
     encrypted_text.join()
 end
 
-puts caeser_cipher("What a string!", 5)
+valid_input = false
+input_string = ""
+shift = ""
+
+puts "Type the sentance you want to encrypt and press enter."
+input_string = gets.chomp
+
+
+until valid_input
+    puts "Type the shift amount and press enter. (Must be a number between 1-99)"
+    shift = gets.chomp
+    shift.match(/^[1-9]{1}\d?$/) ? valid_input = true : valid_input = false
+    puts "#{shift} is not a number between 1-99" unless valid_input
+end
+
+shift = shift.to_i
+
+puts caeser_cipher(input_string, shift)
+
 
