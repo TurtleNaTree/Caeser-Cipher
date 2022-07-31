61954bb249d622915349a870ff8337ed10333e07
TurtleNaTree <kmcqueen108121@hotmail.com>
2022-07-31 09:52:28 +0000

    added pry-byebyg for debugging and added code to do negative shifts

--- caeser_cipher.rb
+++ caeser_cipher.rb
@@ -1,12 +1,25 @@
+require "pry"
+
 
 def caeser_cipher(text, shift)
     encrypted_text = text.split(//).map do |char|
-        if char === char.capitalize
-            char.match(/[A-Z]/) ? char.setbyte(0, char.getbyte(0) + shift > 90 ? char.getbyte(0) + shift - 90 + 64 : char.getbyte(0) + shift) : char
-            char
-        else 
-            char.match(/[a-z]/) ? char.setbyte(0, char.getbyte(0) + shift > 122 ? char.getbyte(0) + shift - 122 + 96 : char.getbyte(0) + shift) : char
-            char
+        case 
+        when (shift > 0)  
+            if char === char.capitalize
+                char.match(/[A-Z]/) ? char.setbyte(0, char.getbyte(0) + shift > 90 ? char.getbyte(0) + shift - 90 + 64 : char.getbyte(0) + shift) : char  
+                char
+            else 
+                char.match(/[a-z]/) ? char.setbyte(0, char.getbyte(0) + shift > 122 ? char.getbyte(0) + shift - 122 + 96 : char.getbyte(0) + shift) : char
+                char
+            end
+        when (shift < 0)
+            if char === char.capitalize
+                char.match(/[A-Z]/) ? char.setbyte(0, char.getbyte(0) + shift < 65 ? char.getbyte(0) + shift + 26 : char.getbyte(0) + shift) : char
+                char
+            else 
+                char.match(/[a-z]/) ? char.setbyte(0, char.getbyte(0) + shift < 97 ? char.getbyte(0) + shift + 26 : char.getbyte(0) + shift) : char
+                char
+            end
         end
     end
     encrypted_text.join()
@@ -23,12 +36,12 @@ input_string = gets.chomp
 until valid_input
     puts "Type the shift amount and press enter. (Must be a number between 1-99)"
     shift = gets.chomp
-    shift.match(/^[1-9]{1}\d?$/) ? valid_input = true : valid_input = false
+    shift.match(/^-?[1-9]{1}\d?$/) ? valid_input = true : valid_input = false
     puts "#{shift} is not a number between 1-99" unless valid_input
 end
 
 shift = shift.to_i
-
-puts caeser_cipher(input_string, shift)
+puts shift.to_i
+puts "Text changed to #{caeser_cipher(input_string, shift)}"
 
 
